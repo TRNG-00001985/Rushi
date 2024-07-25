07a1370d54f92c257491cb7af51c28f0b4b07c36
Rushi <116496246+Rushipasumarthi@users.noreply.github.com>
2024-07-25 10:40:27 +0530

    Add files via upload

---
+++ arraysub.java
@@ -0,0 +1,22 @@
+public class arraysub 
+{
+    public static void main(String[] args) 
+    {
+		int arr[]= {1,5,3,7,9,4};
+		int n = arr.length;
+		for(int i=0;i<n;i++) 
+        {
+			
+			for(int j=0;j<n;j++) 
+            {
+				
+				for(int k=i;k<=j;k++) 
+                {
+					System.out.print(arr[k]);
+				}
+				System.out.println();
+			}
+		}
+	}
+    
+}
